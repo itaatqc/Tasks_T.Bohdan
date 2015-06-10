8726dc669320cefbfb3bc2b76caab87af09c09c1
Sandman148 <andrew.bryhas@gmail.com>
2015-06-11 02:39:38 +0300

    Create SendMail.java

---
+++ mailrutest/src/main/java/com/bryhas/project/mailrutest/tools/SendMail.java
@@ -0,0 +1,58 @@
+package com.bryhas.project.mailrutest.tools;
+
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+/**
+ * Author: Andrii Bryhas
+ * Date: 10.06.15
+ */
+
+public class SendMail {
+	
+	//this class sends email from @username to @recipient
+
+    public static void sendMailToInbox(String subject, String text) {
+
+        final String username = "tsiberbiller@mail.ru";
+        final String password = "heilmeinfuhrer";
+        final String recipient = "tsiberbiller@mail.ru";
+
+        Properties props = new Properties();
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.host", "smtp.mail.ru");
+        props.put("mail.smtp.port", "587");
+
+        Session session = Session.getInstance(props,
+          new javax.mail.Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(username, password);
+            }
+          });
+
+        try {
+
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(username));
+            message.setRecipients(Message.RecipientType.TO,
+                InternetAddress.parse(recipient));
+            message.setSubject(subject);
+            message.setText(text);
+
+            Transport.send(message);
+
+            System.out.println("Done");
+
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
